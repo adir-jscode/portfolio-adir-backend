c66f5a75f8e489cafee8f9aebab013d5d58337e4
adir-jscode <adir.jscode@gmail.com>
2025-10-06 11:04:54 +0600

    server setup done

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({ success: true, message: "Welcome to adir's backend server" });
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,59 @@
+import { Server } from "http";
+import app from "./app";
+let server: Server;
+
+const startServer = async () => {
+  try {
+    server = app.listen(process.env.PORT || 5000, () => {
+      console.log(
+        "✅ Portfolio Server is running on port " + (process.env.PORT || 5000)
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled rejection detected..Server is shutting down", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log("uncaught Exception detected..Server shutting down..", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//cloud related error
+process.on("SIGTERM", (err) => {
+  console.log("SIGTERM Signal Received..Server shutting down..", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+
+//manual server off
+process.on("SIGINT", (err) => {
+  console.log("SIGINT Signal Received..Server shutting down..", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
